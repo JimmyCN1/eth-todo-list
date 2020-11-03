f61df4707927b150c0416d2a67d4af8d45f8290b
JimmyCN1 <jimmy.c.njoku@gmail.com>
2020-11-03 23:13:20 +1100

    get todolist from local eth blockchain

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,7 +1,22 @@
 pragma solidity >=0.4.22 <0.8.0;
 
 contract TodoList {
-  uint public taskCount = 0;
+    uint256 public taskCount = 0;
 
+    struct Task {
+        uint256 id;
+        string content;
+        bool completed;
+    }
 
+    mapping(uint256 => Task) public tasks;
+
+    constructor() public {
+      createTask("check out dappuniversity.com");
+    }
+
+    function createTask(string memory content) public {
+      taskCount++;
+      tasks[taskCount] = Task(taskCount, content, false);
+    }
 }
